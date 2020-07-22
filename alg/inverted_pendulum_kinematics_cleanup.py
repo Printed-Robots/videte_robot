1deebc8d9f56470929e01e18c0608b80330c7f03
Johannes Jeising <Johannes.Jeising@gmail.com>
2020-07-23 00:21:44 +0200

    inverted pendulum kinematics seem reasonable

--- alg/inverted_pendulum_kinematics_cleanup.py
+++ alg/inverted_pendulum_kinematics_cleanup.py
@@ -1,7 +1,6 @@
 import tqdm
 import math
-import scipy.linalg
-import scipy.signal
+import scipy.integrate as integrate
 import matplotlib.pyplot as plt
 import numpy as np
 import sympy
@@ -30,21 +29,21 @@ l_ow_iw = r_2 - r_1
 g = sympy.symbols('g')
 
 # Gear ratio
-i = r_2 / r_1
+i_gear = r_2 / r_1
 
 # Linking rotation of outer and inner wheel
 φ_iw = φ
-φ_ow = (1/i) * φ_iw
+φ_ow = (1/i_gear) * φ_iw
 
 ω = φ.diff(t)
 ω_iw = ω
-ω_ow = (1/i) * ω_iw
+ω_ow = (1/i_gear) * ω_iw
 
 
 # Y positions
 y_ow = r_3 + y
 y_iw = y_ow - l_ow_iw * sympy.cos(ρ)
-y_m = y_iw + l_iw_m * sympy.cos(θ)
+y_m = y_iw - l_iw_m * sympy.cos(θ)
 
 
 # Rotation based x movement
@@ -87,19 +86,18 @@ W_m = (1/2) * (sympy.diff(x_m, t)**2 + sympy.diff(y_m, t)**2)
 d_ow, d_iw, d_m, d_owr, d_iwr = sympy.symbols(
     'd_ow d_iw d_m d_owr d_iwr')
 
-W_d_ow = d_ow * (1/2) * (sympy.diff(x_ow, t)**2 + sympy.diff(y_ow, t)**2)
-W_d_iw = d_iw * (1/2) * sympy.diff(ρ, t)**2
-W_d_m = d_m * (1/2) * sympy.diff(θ, t)**2
-W_d_owr = d_owr * (1/2) * ω_ow**2
-W_d_iwr = d_iwr * (1/2) * ω_iw**2
+W_d_ow = sympy.integrate(
+    d_ow * (1/2) * (sympy.diff(x_ow, t)**2 + sympy.diff(y_ow, t)**2), t)
+W_d_iw = sympy.integrate(d_iw * (1/2) * sympy.diff(ρ, t)**2, t)
+W_d_m = sympy.integrate((d_m * (1/2) * sympy.diff(θ, t)**2), t)
+W_d_owr = sympy.integrate(d_owr * (1/2) * ω_ow**2, t)
+W_d_iwr = sympy.integrate(d_iwr * (1/2) * ω_iw**2, t)
 
 W_heat = W_d_ow + W_d_iw + W_d_m + W_d_owr + W_d_iwr
-# Currently ignored
-# W_heat = 0
 
 
 # Kinetic Energy
-T = W_owr + W_iwr + W_ow + W_iw + W_m - W_heat
+T = W_owr + W_iwr + W_ow + W_iw + W_m + W_heat
 
 
 ###############
@@ -180,35 +178,63 @@ lengths = {
 }
 
 dampening = {
-    d_ow: 0.01,
-    d_iw: 0.01,
-    d_m: 0.01,
-    d_owr: 0.01,
-    d_iwr: 0.01
+    d_ow: 1.0,
+    d_iw: 1.0,
+    d_m: 1.0,
+    d_owr: 1.0,
+    d_iwr: 1.0
 }
 
-Jacobian = A_lin.subs(masses).subs(lengths).subs(dampening)
+Jacobian = np.float64(A_lin.subs(masses).subs(lengths).subs(dampening))
 
 # Simulation
-x_0 = sympy.Matrix([1.0, 0.0, 0.15, 0.2, 0.0, -0.1, 0.2, 0.0])
+x_0 = np.float64([1.0, 0.0, 0.1, 1.0, 0.14, 0.23, 0.0, 0.1])
 
 
 dt = 0.01
 
-timeline = np.arange(0.0, 5, dt)
-result = sympy.zeros(len(x_0), len(timeline))
+timeline = np.arange(0.0, 10.0, dt)
 
-result[:, 0] = x_0
 
-# states = [y, y.diff(t), θ, θ.diff(t), ρ, ρ.diff(t), φ, φ.diff(t)]
+def applyJ(y, t):
+    return Jacobian.dot(y)
 
-for i in tqdm.tqdm(range(len(timeline) - 1)):
-    result[:, i + 1] = Jacobian * result[:, i] * dt + result[:, i]
 
-for row in tqdm.tqdm(range(len(x_0))):
-    plt.plot(timeline, result.row(row).T, label='J (' + str(row) + ')')
+solution = integrate.odeint(applyJ, x_0, timeline)
 
-plt.axis([-0.01, 5, -1, 2])
-plt.legend()
+# x_ow, y_ow, x_iw, y_iw, x_m, y_m
+positions = sympy.zeros(6, len(timeline))
 
+for tp in tqdm.tqdm(range(len(timeline))):
+    results = {
+        y: solution[tp, 0],
+        φ: solution[tp, 6],
+        ρ: solution[tp, 4],
+        θ: solution[tp, 2]
+    }
+    positions[0, tp] = x_ow.subs(lengths).subs(results)
+    positions[1, tp] = y_ow.subs(lengths).subs(results)
+    positions[2, tp] = x_iw.subs(lengths).subs(results)
+    positions[3, tp] = y_iw.subs(lengths).subs(results)
+    positions[4, tp] = x_m.subs(lengths).subs(results)
+    positions[5, tp] = y_m.subs(lengths).subs(results)
+
+fig, axs = plt.subplots(2)
+
+for row, state in enumerate(states):
+    axs[0].plot(timeline, solution[:, row], label=str(state))
+
+axs[1].plot(timeline, positions[0, :].T, label='x_ow')
+# axs[1].plot(timeline, positions[1, :].T, label='y_ow')
+axs[1].plot(timeline, positions[2, :].T, label='x_iw')
+axs[1].plot(timeline, positions[3, :].T, label='y_iw')
+axs[1].plot(timeline, positions[4, :].T, label='x_m')
+axs[1].plot(timeline, positions[5, :].T, label='y_m')
+
+axs[0].legend()
+axs[1].legend()
+plt.xlabel('t')
+axs[0].grid()
+axs[1].grid()
+# plt.axis([-0.01, 5, -0.75, 1.5])
 plt.show()
